8cadaf54300968f117935f92cdac580e79bbacd3
chimexXy <moadzahnoun37@gmail.com>
2025-02-11 03:07:37 +0100

    chimex

--- rotate_utils.c
+++ rotate_utils.c
@@ -6,7 +6,7 @@
 /*   By: mozahnou <mozahnou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:21:16 by mozahnou          #+#    #+#             */
-/*   Updated: 2025/02/11 02:32:12 by mozahnou         ###   ########.fr       */
+/*   Updated: 2025/02/11 03:07:19 by mozahnou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,27 @@
 void rotate_a(t_list **stack_a)
 {
     t_list *tmp;
-    t_list *last;
 
     if (*stack_a == NULL || (*stack_a)->next == NULL)
         return;
 
     tmp = *stack_a;
-    *stack_a = (*stack_a)->next;
-    last = *stack_a;
-    
-    while (last->next != NULL)
-        last = last->next;
-
-    last->next = tmp;
     tmp->next = NULL;
+    *stack_a = (*stack_a)->next;
+	lst_add_back(stack_a, tmp);
 }
 
 void rotate_b(t_list **stack_b)
 {
     t_list *tmp;
-    t_list *last;
 
-    if (*stack_b == NULL || (*stack_b)->next == NULL)
+    if (*stack_a == NULL || (*stack_a)->next == NULL)
         return;
 
     tmp = *stack_b;
-    *stack_b = (*stack_b)->next;
-    last = *stack_b;
-    
-    while (last->next != NULL)
-        last = last->next;
-
-    last->next = tmp;
     tmp->next = NULL;
+    *stack_b = (*stack_b)->next;
+	lst_add_back(stack_b, tmp);
 }
 
 void rotate_a_b(t_list **stack_a, t_list **stack_b)
