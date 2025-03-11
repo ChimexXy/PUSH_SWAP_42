384fbc9c9740aabf25ea6bd9440ab465543c2170
chimexXy <moadzahnoun37@gmail.com>
2025-03-11 23:55:15 +0000

    chimex

--- ft_ret_lst.c
+++ ft_ret_lst.c
@@ -6,7 +6,7 @@
 /*   By: mozahnou <mozahnou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 12:02:43 by mozahnou          #+#    #+#             */
-/*   Updated: 2025/03/07 02:48:11 by mozahnou         ###   ########.fr       */
+/*   Updated: 2025/03/11 23:53:23 by mozahnou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,28 @@ void	free_double_pointer(char **array)
 	array = NULL;
 }
 
+int check_ret(char **ret)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while(ret[j])
+	{
+		i = 0;
+		while(ret[j][i])
+		{
+			if ((ret[j][i] == '-' && ret[j][i + 1] == '\0') 
+				|| (ret[j][i] == '+' && ret[j][i + 1] == '\0'))
+				return (0);
+			i++;
+		}
+		j++;
+	}
+	return (1);
+}
+
 t_list	*ft_ret_lst(char **ret)
 {
 	int		ind;
@@ -39,10 +61,11 @@ t_list	*ft_ret_lst(char **ret)
 	ind = 0;
 	data = 0;
 	head = NULL;
+	
 	while (ret[ind])
 	{
 		data = ft_atoi(ret[ind], &val);
-		if (val == 1)
+		if (val == 1 || check_ret(ret) == 0)
 		{
 			write(2, "Error\n", 6);
 			free_double_pointer(ret);
